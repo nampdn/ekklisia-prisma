950eac5f28c7ff2c03142bb819a0123b3ed11f6d
nampdn <nampdn@protonmail.com>
2019-12-31 16:30:00 +0700

    fix(dotenv): load env from .env file

--- src/context.ts
+++ src/context.ts
@@ -1,5 +1,9 @@
 import { Photon } from '@prisma/photon'
 import { ContextParameters } from 'graphql-yoga/dist/types'
+import * as dotenv from 'dotenv'
+
+dotenv.config() // load the environment variables
+
 const photon = new Photon()
 
 export interface Context {
